2ed98d1f5519d35c47f6e4c7138931db68d89a64
David Khudaverdyan <david.khudaverdyan@bentley.com>
2024-04-14 00:40:00 +0200

    add pillow + crop scnreenshots

--- InstScrapper/InstScrapper.py
+++ InstScrapper/InstScrapper.py
@@ -5,37 +5,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
-
 import time
-# Path to your Chrome profile
+import os
+
 chrome_profile_path = r"C:\Users\David.Khudaverdyan\AppData\Local\Google\Chrome\User Data - Copy"
-url_instagram_profile = r"https://www.instagram.com/kunzhut_kunzhut/"
-
-
+working_dir = r"D:/temp/inst/"
+temp_screenshot_file_name = "full_screenshot.png"
+instagram_profile_name = r"kunzhut_kunzhut"
+url_instagram = r"https://www.instagram.com/" 
+first_picture_coords = [760, 380]
 
 def take_screenshot(driver, x_min, x_max, y_min, y_max, filename):
-    # Take a full-page screenshot and then crop it to the desired area
-    driver.save_screenshot(filename)
-    # from PIL import Image
-    # image = Image.open("full_screenshot.png")
-    # cropped_image = image.crop((x_min, y_min, x_max, y_max))
-    # cropped_image.save(filename)
+    temp_screenshot_path = working_dir + temp_screenshot_file_name
+    driver.save_screenshot(temp_screenshot_path)
+    from PIL import Image
+    image = Image.open(temp_screenshot_path)
+    cropped_image = image.crop((x_min, y_min, x_max, y_max))
+    cropped_image.save(working_dir + filename)
+    
+def create_directory_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def navigate_and_capture(driver, x_min, x_max, y_min, y_max, iterations):
-    # Set up the WebDriver
-
+    create_directory_if_not_exists(working_dir + instagram_profile_name)
     for i in range(iterations):
         screenshot_filename = f"screenshot_{i}.png"
         take_screenshot(driver, x_min, x_max, y_min, y_max, screenshot_filename)
-        print(f"Screenshot {i} saved.")
-        
-        # Navigate forward
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ARROW_RIGHT)
-        
-        # Wait a bit for the page to load/update
-        time.sleep(0.5)  # Adjust this delay as necessary for page load times
-        
-    driver.quit()
+    # driver.quit()
     
 def create_driver():
   # Create ChromeOptions object
@@ -51,16 +49,16 @@ def create_driver():
   return driver
 
 def open_instagram_profile(driver):
-  driver.get(url_instagram_profile)
+  driver.get(url_instagram + instagram_profile_name)
   sleep(1)
 
 def click_on_first_picture(driver):
   action = ActionChains(driver)
-  action.move_by_offset(760, 380).perform()
+  action.move_by_offset(first_picture_coords[0], first_picture_coords[1]).perform()
   action.click().perform()
   
 
 driver = create_driver();
 open_instagram_profile(driver)
 click_on_first_picture(driver)
-# navigate_and_capture(driver, 100, 800, 100, 600, 5)
+navigate_and_capture(driver, 100, 800, 100, 600, 5)
